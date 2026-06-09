1ee4fb93104cd54450c146234e5fd7a09bee0067
彭明亮 <2682045297@qq.com>
2026-06-09 18:32:00 +0800

    '+++'

--- admin/edge-functions/api/stats.js
+++ admin/edge-functions/api/stats.js
@@ -1,4 +1,6 @@
-export default async function onRequest({ request, env }) {
+export default async function onRequest(context) {
+    const { request, env } = context
+
     const headers = {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
